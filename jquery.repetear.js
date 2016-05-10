fdd1e7e7a00edadb50e761cef0b881b73d13fb48
elib0 <elijose.c@gmail.com>
2016-05-10 16:20:06 -0430

    Nueva opcion si usa form validate

--- jquery.repetear.js
+++ jquery.repetear.js
@@ -8,13 +8,25 @@
         	elementClass:'element',
             addBtnClass:'.add',
             remBtnClass:'.remove',
-            tplToRepeat: '.template'
+            tplToRepeat: '.template',
+            formValidation: true
         }, options );
 
 	   	$this.on('click', opt.addBtnClass, function(){
 	   		var template = $this.find(opt.tpl)[0],
 	   			$new=template.nodeName=='SCRIPT'?$(template).tmpl():$(template).clone().removeClass(opt.tpl);
 	   			$new.addClass('.'+elementClass).insertBefore(template);
+
+	   		if (opt.formValidation) { //Si usa form validate
+	   			var $form = $(this).closest('form');
+	   			$new.find('[disabled]').prop('disabled',false);
+	   			if($.fn.validator&&$form.data('bs.validator')) $form.validator('reloadFields');
+	   			if($.fn.formValidation&&$form.data('formValidation')){
+	   				$new.find('input,textarea,select').filter(':visible').each(function(){
+	   					$form.formValidation('addField',this.name);
+	   				});
+	   			}
+	   		}
 	   		//Agregamos nuevo el nuevo elemento al retorno
 	   		response.addedElement = $new;
 	    }).on('click', '.'+elementClass+' '+opt.remBtnClass, function(){
