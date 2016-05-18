22ecaab9b9a4dae01dd810f0f0a48950fd1b6ec6
elib0 <elijose.c@gmail.com>
2016-05-18 12:48:50 -0430

    Varios arreglos al clonar, al eliminar items, al eliminar items clonados y hay multiples repeter con misma clase, otras correciones menores

--- src/jquery.repeter.js
+++ src/jquery.repeter.js
@@ -14,13 +14,12 @@
 	            addBtnClass:'.add',
 	            remBtnClass:'.remove',
 	            tplClass: '.template',
-	            formValidation: false,
 	            formSelector: 'form',
 	            elements: elementOptions
 	        }, options );
 		var elementClass = opt.elements.class || elementOptions.class;
 	   	$this.on('click', opt.addBtnClass, function(){
-	   		var $root = $(this).closest( '.'+$this.attr('class') );
+	   		var $root = $(this).closest( '.'+$this.attr('class').split(' ').join('.') );
 	   		add($root);
 	    }).on('click', '.'+(elementClass)+' '+opt.remBtnClass, function(){
 	    	remove(this);
@@ -47,7 +46,7 @@
 	    	if (!opt.elements.insertIn || opt.elements.mirrorIn) $new.insertBefore(template);
 
 	    	// Si usa "Bootstrap Validator" http://1000hz.github.io/bootstrap-validator/
-	    	if (opt.formValidation) {
+	    	if ($.fn.formValidation || $.fn.validator) {
 	    		var $form = $($root).closest(opt.formSelector);
 	    		$new.find('[disabled]').prop('disabled',false);
 	    		if($.fn.validator&&$form.data('bs.validator')) $form.validator('reloadFields');
@@ -62,21 +61,21 @@
 	    }
 
 	    var remove = function(ele){
-	    	var $root = $(ele).closest( '.'+$this.attr('class') );
+	    	var $root = $(ele).closest( '.'+$this.attr('class').split(' ').join('.') );
 	    	ele = ( (typeof ele) == 'number' )?$('button.remove','r-ele-'+ele): $(ele,$root);
 	    	ele.closest('.element').fadeOut(250,function(){
 	    		var rEleNum = $(this).data('r-ele'),
 	    			numElements = $('.'+elementClass,$root).length;
 
 	    		//Foco en penultimo elemento de formulario
-				if (opt.formValidation) ele.prev().find('select,input,textarea,button').last().focus();
+				if ($.fn.formValidation || $.fn.validator) ele.prev().find('select,input,textarea,button').last().focus();
 
-				$('.r-ele-'+rEleNum, $root).remove();	//Elimina Clones
-				$(this).remove();						//Elimina elemento
+				if(opt.elements.mirrorIn) $(opt.elements.mirrorIn, $root).find('.r-ele-'+rEleNum).remove();	//Elimina Clones
+				$(this).remove();																//Elimina elemento
 
 				//Enumera elementos adyacentes nuevamente
 				for (var i = rEleNum+1; i <= numElements; i++) {
-					$('.r-ele-'+i, $this).data('r-ele', i-1).removeClass('r-ele-'+i).addClass('r-ele-'+(i-1));
+					$('.r-ele-'+i, $root).data('r-ele', i-1).removeClass('r-ele-'+i).addClass('r-ele-'+(i-1));
 				}
 				//Respuesta al borrar elemento
 		   		return {parent: $root,addedElement:null,removedElement:ele};
